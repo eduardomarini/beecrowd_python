fc63475b23590ad8ee74497636d356acda5d57f4
eduardomarini <eduardoribeiromarini@gmail.com>
2023-12-19 23:13:45 -0300

    Problema 1008

---
+++ 1008.py
@@ -0,0 +1,10 @@
+numero_func = int(input())
+h_trabalhadas = int(input())
+valor_hora = float(input())
+
+salario = float(h_trabalhadas * valor_hora)
+
+print(
+    f'NUMBER = {numero_func}' '\n'
+    f'SALARY = U$ {salario:.2f}'
+      )
